eb6cc87a18bb7d55561588245daf975127ae1b37
Sean McMahon <seantmcmahon95@gmail.com>
2018-01-04 01:24:00 +0000

    All items added to firstscreen of graph creation and customised for now.

--- src/root/nested/visualisation.py
+++ src/root/nested/visualisation.py
@@ -11,14 +11,25 @@ from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.dropdown import DropDown
 from kivy.core.window import Window
+from kivy.properties import ListProperty
+from kivy.core.text import LabelBase
 
 Builder.load_file('visualisationScreen.kv')
 
 class DataSelectScreen(Screen):
+    
     def __init__(self, **kwargs):
-        self.dropdown_height = Window.size[1] * 0.1
+        self.unchecked_colour = [0.7, 0, 0, 1]
+        self.checked_colour = [0, 0.7, 0, 1]
         super(DataSelectScreen, self).__init__()
-    pass
+    
+    def second_graph_toggled(self):   
+        if self.layout.second_graph.state == 'down':
+            self.layout.second_graph.background_color = self.checked_colour
+            self.layout.second_graph.text = unichr(8730)
+        else:
+            self.layout.second_graph.background_color = self.unchecked_colour
+            self.layout.second_graph.text = 'x'
 
 class DataViewScreen(Screen):
     pass
